602148234f20f0fc2b753f76134881d4bad612da
akcgjc007 <akcgjc007@gmail.com>
2021-03-27 22:21:25 +0530

    updated the funding script

--- scripts/fund-contract.js
+++ scripts/fund-contract.js
@@ -1,5 +1,5 @@
-const MyContract = artifacts.require('MyContract')
-const LinkTokenInterface = artifacts.require('LinkTokenInterface')
+const MyContract = artifacts.require("DnDc");
+const ERC20 = require("@openzeppelin/contracts/build/contracts/ERC20.json");
 
 /*
   This script is meant to assist with funding the requesting
@@ -8,17 +8,22 @@ const LinkTokenInterface = artifacts.require('LinkTokenInterface')
   can be retrieved by calling the withdrawLink() function.
 */
 
-const payment = process.env.TRUFFLE_CL_BOX_PAYMENT || '1000000000000000000'
-
-module.exports = async callback => {
+module.exports = async (callback) => {
   try {
-    const mc = await MyContract.deployed()
-    const tokenAddress = await mc.getChainlinkToken()
-    const token = await LinkTokenInterface.at(tokenAddress)
-    console.log('Funding contract:', mc.address)
-    const tx = await token.transfer(mc.address, payment)
-    callback(tx.tx)
+    const mc = await MyContract.deployed();
+    const amount = (10 ** 18).toString();
+    const tokenAddress = "0x01be23585060835e02b77ef475b0cc51aa1e0709";
+    const token = new web3.eth.Contract(ERC20.abi, tokenAddress);
+    console.log("Funding contract name:", mc.contractName);
+    console.log("Funding contract addr:", mc.address);
+    myAccount = (await web3.eth.getAccounts())[0];
+    // const tx = await token.methods.transfer(mc.address, amount).send({from: myAccount});
+    // console.log(tx);
+
+    console.log("ERC20 balance: ", await token.methods.balanceOf(mc.address).call());
+
+    callback(null);
   } catch (err) {
-    callback(err)
+    callback(err);
   }
-}
+};
--- truffle-config.js
+++ truffle-config.js
@@ -1,46 +1,43 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider')
-require('dotenv').config()
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+require("dotenv").config();
 
-const mnemonic = process.env.MNEMONIC
-const url = process.env.RPC_URL
+const mnemonic = process.env.MNEMONIC;
+const url = process.env.RPC_URL;
 
 module.exports = {
   networks: {
     cldev: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 8545,
-      network_id: '*',
+      network_id: "*",
     },
     ganache: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 7545,
-      network_id: '*',
+      network_id: "*",
     },
     kovan: {
       provider: () => {
-        return new HDWalletProvider(mnemonic, url)
+        return new HDWalletProvider(mnemonic, url);
       },
-      network_id: '42',
-      skipDryRun: true
+      network_id: "42",
+      skipDryRun: true,
     },
     rinkeby: {
       provider: () => {
-        return new HDWalletProvider(mnemonic, url)
+        return new HDWalletProvider(mnemonic, url);
       },
-      network_id: '4',
-      skipDryRun: true
+      network_id: "4",
+      skipDryRun: true,
     },
-
   },
   compilers: {
     solc: {
-      version: '0.6.12',
+      version: "0.6.12",
     },
   },
   api_keys: {
-    etherscan: process.env.ETHERSCAN_API_KEY
+    etherscan: process.env.ETHERSCAN_API_KEY,
   },
-  plugins: [
-    'truffle-plugin-verify'
-  ]
-}
+  plugins: ["truffle-plugin-verify"],
+};
